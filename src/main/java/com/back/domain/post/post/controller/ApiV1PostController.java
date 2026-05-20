9da581d11669a35b59b4f9a84b75f2a1c116a16a
EuniceNam <caellime@gmail.com>
2026-05-20 11:10:51 +0900

    feat: dto 사용 및 jackson 설정

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,9 +1,9 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,18 +15,21 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
-    public List<Post> getItems() {
+    public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
-        return items;
+        return items
+                .stream()
+                .map(PostDto::new) // PostDto로 변환
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Post getItem(
+    public PostDto getItem(
             @PathVariable int id
     ) {
         Post post = postService.findById(id).get();
 
-        return post;
+        return new PostDto(post);
     }
 }
