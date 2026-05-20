719652e757b9daf41f375a2fc4188d8f1752c88e
EuniceNam <caellime@gmail.com>
2026-05-20 12:13:46 +0900

    feat: 공통 응답 객체 RsData 추가

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -4,6 +4,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,7 +49,7 @@ public class ApiV1PostCommentController {
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public String delete(
+    public RsData delete(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -58,6 +59,9 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return "%d번 댓글이 삭제되었습니다.".formatted(id);
+        return new RsData(
+                "200-1",
+                "댓글이 삭제되었습니다."
+        );
     }
 }
